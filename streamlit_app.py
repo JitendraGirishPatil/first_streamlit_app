71a1574e30423671a435e3a037f07ecff9ed3608
Jitendra Girish Patil <120487351+JitendraGirishPatil@users.noreply.github.com>
2023-07-29 23:38:00 +0530

    Update streamlit_app.py
    Read_CSV Pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+y_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
